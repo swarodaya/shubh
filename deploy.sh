7ace09a9a37ac3c2c9ee62eb3bac8023424f6f4a
Tapan Karecha <tapankarecha@gmail.com>
2020-02-29 17:03:53 +0530

    Update copyright and cleanup build.sh

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,3 @@
-# clean up
-trgt="../public"
-rm $trgt/{404.html,favicon.png,index.html,index.xml,logo.png,sitemap.xml}
-if (( $? )); then
-	echo "Error while cleaning up files" >&2
-	exit 1
-else
-	echo "Files cleand up"
-fi
-
-rm -r $trgt/{faq,css,img,js}
-if (( $? )); then
-	echo "Error while cleaning up folders" >&2
-	exit 1
-else
-	echo "Folders cleand up"
-fi
-
 # build
 hugo -d ../public
 
